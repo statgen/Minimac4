30804467faa1c69cd80c68d0f9c16d26f2d377b0
Santy-8128 <sayantan@umich.edu>
2017-09-19 11:46:00 -0400

    Update B38 Chr List

--- src/HaplotypeSet.cpp
+++ src/HaplotypeSet.cpp
@@ -1338,7 +1338,10 @@ bool HaplotypeSet::CheckValidChrom(string chr)
         return true;
 
     string temp[]={"1","2","3","4","5","6","7","8","9","10","11"
-                    ,"12","13","14","15","16","17","18","19","20","21","22","23","X","Y"};
+            ,"12","13","14","15","16","17","18","19","20","21","22","23","X","Y"
+            ,"chr1","chr2","chr3","chr4","chr5","chr6","chr7","chr8","chr9","chr10","chr11"
+            ,"chr12","chr13","chr14","chr15","chr16","chr17","chr18","chr19","chr20"
+            ,"chr21","chr22","chr23","chrX","chrY"};
     std::vector<string> ValidChromList (temp, temp + sizeof(temp) / sizeof(string) );
 
     for(int counter=0;counter<(int)ValidChromList.size();counter++)
