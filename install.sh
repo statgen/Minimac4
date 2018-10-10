d62447554403b86f750906243e474476e0937fc0
David Heise <heiseda@ornl.gov>
2018-10-10 11:49:24 -0400

    Fix a dependency and debug line left behind.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ cget install -f requirements.txt &> install.log
 mkdir release-build
 cd release-build/
 echo -e "Generating MakeFiles ..."
-cmake -DCMAKE_CXX_FLAGS=-pg -DCMAKE_TOOLCHAIN_FILE=../cget/cget/cget.cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../cget/cget/cget.cmake -DCMAKE_BUILD_TYPE=Release ..
 make
 echo "Binary created at /release-build/minimac4"
 
