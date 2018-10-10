8dd2b4f657f8dc98906213864de402443ee8da7e
David Heise <heiseda@ornl.gov>
2018-10-10 11:43:20 -0400

    Changes to support libStatGen with cram support

--- dep/libstatgen.cmake
+++ dep/libstatgen.cmake
@@ -5,7 +5,7 @@ project(libStatGen VERSION 1.0.0)
 
 add_custom_target(libStatGen ALL COMMAND make WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMMENT "Builing libStatGen ...")
 
-file(GLOB_RECURSE LSG_HEADER_LIST "bam/*.h" "fastq/*.h" "general/*.h" "glf/*.h" "samtools/*.h" "vcf/*.h")
+file(GLOB_RECURSE LSG_HEADER_LIST "bam/*.h" "fastq/*.h" "general/*.h" "glf/*.h" "vcf/*.h")
 install(FILES ${LSG_HEADER_LIST} DESTINATION include)
 
 if (BUILD_SHARED_LIBS)
