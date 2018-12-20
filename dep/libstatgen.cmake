79e3ad5b71e55e61060a6c30b73ed544f77f2844
Jonathon LeFaive <jonathonl@users.noreply.github.com>
2018-12-20 16:16:14 +0000

    Fixes dependency build issues.

--- dep/libstatgen.cmake
+++ dep/libstatgen.cmake
@@ -3,7 +3,19 @@ project(libStatGen VERSION 1.0.0)
 
 #execute_process(COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-add_custom_target(libStatGen ALL COMMAND make WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMMENT "Builing libStatGen ...")
+get_property(dirs DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY INCLUDE_DIRECTORIES)
+foreach(dir ${dirs})
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I${dir}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -I${dir}")
+endforeach()
+message("dirs: ${dirs}")
+message("cxxflags: ${CMAKE_CXX_FLAGS}")
+
+set(ENV{CFLAGS}  "${CMAKE_C_FLAGS}")
+set(ENV{CXXFLAGS} "${CMAKE_CXX_FLAGS}")
+set(ENV{LDFLAGS} "-L${CMAKE_PREFIX_PATH}/lib")
+
+add_custom_target(libStatGen ALL COMMAND make CFLAGS="\"${CMAKE_C_FLAGS}\"" WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} COMMENT "Builing libStatGen ...")
 
 file(GLOB_RECURSE LSG_HEADER_LIST "bam/*.h" "fastq/*.h" "general/*.h" "glf/*.h" "samtools/*.h" "vcf/*.h")
 install(FILES ${LSG_HEADER_LIST} DESTINATION include)
