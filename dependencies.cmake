81fab63f10d2e9ea189bd7b2e162f1aee09f59a5
jonathonl <jonathonl@users.noreply.github.com>
2023-02-14 12:01:23 -0500

    Updates binary packaging to use musl.

--- dependencies.cmake
+++ dependencies.cmake
@@ -9,9 +9,9 @@ set(_PREFIX ${CMAKE_ARGV3})
 
 # Make sure this is in the module path
 list(APPEND CMAKE_MODULE_PATH ${CMAKEGET_MODULE_PATH})
-include(./CMakeGet.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/CMakeGet.cmake)
 
 get_filename_component(PREFIX ${_PREFIX} ABSOLUTE)
 # Install recipes
 #cmake_get(pfultz2/cget-recipes PREFIX ${PREFIX} CMAKE_ARGS ${ARGS})
-cmake_get_from(requirements.txt PREFIX ${PREFIX} CMAKE_ARGS ${ARGS})
+cmake_get_from(${CMAKE_CURRENT_LIST_DIR}/requirements.txt PREFIX ${PREFIX} CMAKE_ARGS ${ARGS})
--- package-linux.sh
+++ package-linux.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Usage: docker build -t packaging-ubuntu20 -f packaging-dockerfile-ubuntu20 . 
-#        docker run -v `pwd`:/app -w /app packaging-ubuntu20 ./package-linux.sh
+# Usage: docker build -t packaging-alpine3.17 -f packaging-dockerfile-alpine3.17 . 
+#        docker run -v `pwd`:/app -w /app packaging-alpine3.17 ./package-linux.sh
 set -euo pipefail
 
 src_dir=`pwd`
-build_dir=${src_dir}/pkg-build
+build_dir=${src_dir}/pkg-build-alpine
 
 rm -rf ${build_dir}
 mkdir ${build_dir}
@@ -12,28 +12,33 @@ cd ${build_dir}
 
 export CFLAGS="-fPIC"
 export CXXFLAGS="-fPIC"
-#cget ignore xz
-cget install zlib,http://zlib.net/fossils/zlib-1.2.11.tar.gz
-rm cget/lib/libz.so
-cget install -f ${src_dir}/requirements.txt
+
+cmake -P ../dependencies.cmake deps/ -DSHRINKWRAP_PREFER_STATIC=ON
+rm deps/lib/libz.so
 unset CFLAGS
 unset CXXFLAGS
 
+arc=`uname -m`
 
 cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DCMAKE_TOOLCHAIN_FILE=cget/cget/cget.cmake \
+  -DCMAKE_PREFIX_PATH=$(pwd)/deps \
+  -DCMAKE_CXX_FLAGS="-I$(pwd)/deps/include" \
   -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON \
-  -DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" \
-  -DCPACK_GENERATOR="STGZ;DEB;RPM" \
+  -DCMAKE_EXE_LINKER_FLAGS="-static" \
+  -DCPACK_GENERATOR="STGZ" \
+  -DCPACK_SYSTEM_NAME="Linux-${arc}" \
+  -DCPACK_RESOURCE_FILE_LICENSE=${src_dir}/LICENSE \
   -DCPACK_PACKAGE_CONTACT="csg-devel@umich.edu" \
   ${src_dir}
 
+#-DCMAKE_EXE_LINKER_FLAGS="-static -static-libgcc -static-libstdc++" 
+
 make
-#make manuals
+make manuals
 make package
 
 mkdir -p ${src_dir}/pkg/
-cp minimac4-*.{sh,deb,rpm} ${src_dir}/pkg/
+cp minimac4-*.sh ${src_dir}/pkg/
 cd ${src_dir}
 rm -r ${build_dir}
